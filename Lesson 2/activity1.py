54bbc0b9a44f1c334bd9933928a0626a0ed1a871
anu.is.cool.7@gmail.com <anu.is.cool.7@gmail.com>
2026-07-22 15:15:57 -0700

    activites lesson 2

---
+++ Lesson 2/activity1.py
@@ -0,0 +1,4 @@
+print("Virat Kohli and Tilak Varma are unique players")
+print("Spain won the world cup")
+print("ronaldo is better than messi")
+print("maggi noodles is tasty")
---
+++ Lesson 2/activity2.py
@@ -0,0 +1,13 @@
+x=5
+y="adhithya"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+name=input("What is your name?")
+print(name)
+game=input("what is your fav game")
+if game=="GTA5":
+    print("good choice")
+else:   
+    print("horrible choice")
