bd0241d576347dd47eaa164ae8e0c78e0438a3d5
anu.is.cool.7@gmail.com <anu.is.cool.7@gmail.com>
2026-08-20 15:05:54 -0700

    activities aug 20
    Co-authored-by: Biswareet Panda <biswa.gunu2003@gmail.com>

---
+++ lesson 8/activity3.py
@@ -0,0 +1,17 @@
+# Store the values
+mean1 = 10
+wrong_number = 8
+correct_number = 12
+total_number = 5
+
+# Calculate the wrong total
+sum = mean1 * total_number
+print(sum)
+# Fix the total
+sum2 = sum - wrong_number
+sum2 = sum2 + correct_number
+print(sum2)
+
+# Find the correct mean
+mean2 = sum2 / total_number
+print(mean2)
